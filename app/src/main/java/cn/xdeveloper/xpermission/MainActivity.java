653ab3e9b4d2761f02618c5efa44a0002f2b6d53
a5533348 <318531018@qq.com>
2016-08-23 14:52:34 +0800

    submit v1.0

--- app/src/main/java/cn/xdeveloper/xpermission/MainActivity.java
+++ app/src/main/java/cn/xdeveloper/xpermission/MainActivity.java
@@ -1,37 +1,94 @@
 package cn.xdeveloper.xpermission;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.provider.Settings;
+import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+/**
+ * Demo
+ * Created by Lai on 2016/8/17.
+ */
+public class MainActivity extends XPermissionActivity implements View.OnClickListener {
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_normal);
 
-        findViewById(R.id.btn_normal).setOnClickListener(this);
-        findViewById(R.id.btn_dispatcher).setOnClickListener(this);
-        findViewById(R.id.btn_dispatcher_wrap).setOnClickListener(this);
+        findViewById(R.id.btn_camera).setOnClickListener(this);
+        findViewById(R.id.btn_call).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.btn_normal:
-                startActivity(new Intent(this,NormalPermissionActivity.class));
+        switch (v.getId()) {
+            case R.id.btn_camera:
+
+                requestPermission(new String[]{Manifest.permission.CAMERA}, new PermissionHandler() {
+                    @Override
+                    public void onGranted() {
+                        Intent intent = new Intent(); //调用照相机
+                        intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+                        startActivity(intent);
+                    }
+
+                    @Override
+                    public void onDenied() {
+                        Toast.makeText(MainActivity.this, "拒绝", Toast.LENGTH_SHORT).show();
+                    }
+
+                });
 
                 break;
-            case R.id.btn_dispatcher:
-                startActivity(new Intent(this,PermissionsDispatcherActivity.class));
 
-                break;
-            case R.id.btn_dispatcher_wrap:
-                startActivity(new Intent(this,WrapPermissionsActivity.class));
+            case R.id.btn_call:
+
+                requestPermission(new String[]{Manifest.permission.CALL_PHONE}, new PermissionHandler() {
+                    @Override
+                    public void onGranted() {
+                        Intent intent = new Intent(Intent.ACTION_CALL);
+                        Uri data = Uri.parse("tel:10086");
+                        intent.setData(data);
+                        startActivity(intent);
+                    }
+
+                    @Override
+                    public boolean onNeverAsk() {
+                        new AlertDialog.Builder(MainActivity.this)
+                                .setTitle("权限申请")
+                                .setMessage("在设置-应用-权限中开始电话权限，以保证功能的正常使用")
+                                .setPositiveButton("去开启", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                        Uri uri = Uri.fromParts("package", getPackageName(), null);
+                                        intent.setData(uri);
+                                        startActivity(intent);
+
+                                        dialog.dismiss();
+                                    }
+                                })
+                                .setNegativeButton("取消", null)
+                                .setCancelable(false)
+                                .show();
+
+                        return true;
+                    }
+
+                });
 
                 break;
         }
     }
+
+
 }
---
+++ app/src/main/java/cn/xdeveloper/xpermission/XPermissionActivity.java
@@ -0,0 +1,90 @@
+package cn.xdeveloper.xpermission;
+
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+
+/**
+ * Created by Laiyimin on 2016/8/23.
+ */
+public class XPermissionActivity extends AppCompatActivity {
+
+
+    private PermissionHandler mHandler;
+
+    /**
+     * 请求权限
+     *
+     * @param permissions 权限列表
+     * @param handler     回调
+     */
+    protected void requestPermission(String[] permissions, PermissionHandler handler) {
+        if (PermissionUtils.hasSelfPermissions(this, permissions)) {
+            handler.onGranted();
+        } else {
+            mHandler = handler;
+            ActivityCompat.requestPermissions(this, permissions, 001);
+        }
+    }
+
+
+    /**
+     * permissionResult callback
+     *
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (mHandler == null) return;
+
+        if (PermissionUtils.getTargetSdkVersion(this) < 23 && !PermissionUtils.hasSelfPermissions(this, permissions)) {
+            mHandler.onDenied();
+            return;
+        }
+
+        if (PermissionUtils.verifyPermissions(grantResults)) {
+            mHandler.onGranted();
+        } else {
+            if (!PermissionUtils.shouldShowRequestPermissionRationale(this, permissions)) {
+                if (!mHandler.onNeverAsk()) {
+                    Toast.makeText(this, "权限已被拒绝,请在设置-应用-权限中打开", Toast.LENGTH_SHORT).show();
+                }
+
+            } else {
+                mHandler.onDenied();
+            }
+        }
+    }
+
+
+    /**
+     * 权限回调接口
+     */
+    public abstract class PermissionHandler {
+        /**
+         * 权限通过
+         */
+        public abstract void onGranted();
+
+        /**
+         * 权限拒绝
+         */
+        public void onDenied() {
+        }
+
+        /**
+         * 不再询问
+         *
+         * @return 如果要覆盖原有提示则返回true
+         */
+        public boolean onNeverAsk() {
+            return false;
+        }
+    }
+}
